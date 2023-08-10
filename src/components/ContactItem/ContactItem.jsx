6bd8553c7c71da11bb77f761de581e0dbdbe84e3
Kamilucha <brnkamila2@gmail.com>
2023-08-10 17:57:13 +0300

    Update ContactItem.jsx

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -5,7 +5,7 @@ import { Item, Text } from './ContactItem.styled'
 import { deleteContactThunk } from 'redux/thunk';
 
 export const ContactItem = ({ name, number, id }) => {
-    console.log(id)
+    console.log(id, number)
 
     const dispatch = useDispatch();
 
